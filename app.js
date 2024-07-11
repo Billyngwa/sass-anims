81680e83115d36823cbafbb2f291b3166f74a950
Billyngwa <billytatangwa5@gmail.com>
2024-07-11 09:35:11 +0100

    implement fetch

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const api = 'http://localhost:4200';
+const base_url = 'http://localhost:3000/auth/signup';
 
-fetch(api,{
-    method:"GET"
-}).then(response => response.json())
-.then(data =>{
-    
-console.log(data);
-})    
-.catch(error => {
-    console.log(error);
-})
+const registerForm = document.querySelector('.sign-up-form');
+if (registerForm) {
+
+    registerForm.addEventListener('submit', e => {
+
+        e.preventDefault();
+        let data = new FormData(registerForm);
+        let jdata = {};
+        data.forEach((value, key) => {
+            jdata[key] = value;
+
+        })
+        console.log(jdata);
+        fetch(base_url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jdata),
+            mode: "cors",
+            cache: "no-cache",
+        })
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem("token", data.token)
+                console.log(data);
+                window.location.assign("./pages/signin.html");
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    });
+}
+
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,3 +9,19 @@ second.forEach(paragraph =>{
 mobileNav.classList.toggle('active');
 
 })
+
+const localInfo = JSON.parse(localStorage.getItem("userInfo"));
+const dp = document.getElementById("dp");
+const dpName = document.getElementById("profilename");
+const dpEmail = document.getElementById("profileemail");
+const changeImage = document.querySelector(".changeImage");
+dpName.textContent = localInfo.data.firstName;
+dpEmail.textContent = localInfo.data.email
+
+dp.src = localInfo.data.imageUrl
+const ut = dp.src;
+console.log("dp",ut);
+
+changeImage.addEventListener("click", (e)=> {
+    alert("could not change image")
+})
